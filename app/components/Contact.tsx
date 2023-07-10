aab3e1c826547ead003dbc8affbaae2cb299aa22
Haruki Koh <harukikoh@gmail.com>
2023-07-10 23:13:50 +1000

    Adjusted gaps between social media logos when in mobile view

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -7,11 +7,11 @@ export default function Contact() {
         <div className="contact section bg-neutral" id="contact">
             <div className="container mx-auto">
                 <h1 className="title mb-8 text-center tracking-wide font-semibold uppercase">Get In Touch</h1>
-                <p className="text-center">My inbox is always open! Feel free to shoot me an email or connect on LinkedIn.</p>
+                <p className="text-center px-4 md:px-0">My inbox is always open! Feel free to shoot me an email or connect on LinkedIn.</p>
             </div>
 
             <div className="socials mt-12 text-content">
-                <ul className="flex flex-row justify-center gap-16 text-xl">
+                <ul className="flex flex-row flex-wrap px-4 md:px-0 justify-center gap-4 md:gap-16 text-xl">
                     {data.socials.map((social: string, i: number) => {
                         return (
                             <SocialItem 
