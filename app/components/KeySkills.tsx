96fa606d1e6a8db205ddd5cc0373a2cb83addd7d
Haruki Koh <harukikoh@gmail.com>
2023-07-16 15:30:05 +1000

    Fixed props passed to the Technology component

--- app/components/KeySkills.tsx
+++ app/components/KeySkills.tsx
@@ -1,5 +1,5 @@
 import data from '@/json/data.json'
-import IconItem from './Technology';
+import Technology from './Technology';
 
 export default function KeySkills() {
     const SIZE = 40;
@@ -8,7 +8,7 @@ export default function KeySkills() {
                 <div className="flex flex-row flex-wrap gap-x-8 gap-y-4">
                     {data.languages.map((language: string, i: number) => {
                         return (
-                            <IconItem src={data.icons[language]} language={language} width={SIZE} key={i}/>
+                            <Technology language={language} key={i}/>
                         )
                     })}
                 </div>
